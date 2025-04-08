3ddfbd7e8df152fe7cf5b7f7da83afa5567d1bd6
Khadka <120006828+khadka27@users.noreply.github.com>
2025-04-08 12:03:07 +0545

    Add image domain configuration to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["localhost", "daily-health-suppliment.vercel.app"], // Add the domain from where images are served
+  },
 };
 
 export default nextConfig;
