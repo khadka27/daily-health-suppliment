037a5ab962e62405b5f96c87c8c37e086dea54a6
DiwasGurung <119999536+DiwasGurung@users.noreply.github.com>
2025-05-29 17:43:17 +0545

    complete

--- src/app/(app)/Article/[id]/page.tsx
+++ src/app/(app)/Article/[id]/page.tsx
@@ -3,13 +3,13 @@ import ArticleWrapper from "@/components/ArticleWrapper"
 export default async function ArticlePage({
   params,
 }: {
-  params: { id: string }
+  params:Promise< { id: string }>
 }) {
   return (
     <div className="min-h-screen bg-white">
       <main className="py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <ArticleWrapper id={params.id} />
+          <ArticleWrapper id={(await params).id} />
         </div>
       </main>
     </div>
