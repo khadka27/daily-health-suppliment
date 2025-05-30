b2bc5ea3dbc2818effcf37e2718c0296e63bc6b3
DiwasGurung <119999536+DiwasGurung@users.noreply.github.com>
2025-05-30 15:02:31 +0545

    remove unsupported components prop from Calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,7 +2,6 @@
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -54,10 +53,7 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+      // Removed unsupported 'components' prop as 'IconLeft' and 'IconRight' are not valid
       {...props}
     />
   )
